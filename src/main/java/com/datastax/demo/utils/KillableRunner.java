20add4061a304cd5e0c7d0a1763ea76446fe648a
Simon Ambridge <simon@ntier-db.com>
2016-11-20 00:04:59 +0000

    Working on ReST...

--- src/main/java/com/datastax/demo/utils/KillableRunner.java
+++
@@ -1,6 +0,0 @@
-package com.datastax.demo.utils;
-
-public interface KillableRunner extends Runnable {
-
-	public void shutdown();
-}
--- src/main/java/com/datastax/demo/utils/PropertyHelper.java
+++
@@ -1,8 +0,0 @@
-package com.datastax.demo.utils;
-
-public class PropertyHelper {
-	
-	public static String getProperty(String name, String defaultValue){		
-		return System.getProperty(name) == null ? defaultValue : System.getProperty(name); 
-	}	
-}
