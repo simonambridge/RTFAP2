894c43a6980d2975cae7bb110c4d9ea54c3bb81d
Simon Ambridge <simon@ntier-db.com>
2016-12-09 15:24:17 +0000

    DOc updates

---
+++ Solr/build_solr_indexes.sh
@@ -0,0 +1,5 @@
+dsetool create_core rtfap.transactions generateResources=true reindex=true
+dsetool create_core rtfap.txn_count_min generateResources=true reindex=true
+dsetool reload_core rtfap.txn_count_min schema=./txn_count_min.xml reindex=true
+
+
