766550424cd709c2214400b2af79c5fe9ac01e25
Simon Ambridge <simon@ntier-db.com>
2017-11-12 00:07:20 +0000

    Updates to ReST README

--- Solr/build_solr_indexes.sh
+++ Solr/build_solr_indexes.sh
@@ -1,5 +1,5 @@
 dsetool create_core rtfap.transactions generateResources=true reindex=true
+dsetool reload_core rtfap.transactions schema=./transactions.xml reindex=true
+
 dsetool create_core rtfap.txn_count_min generateResources=true reindex=true
 dsetool reload_core rtfap.txn_count_min schema=./txn_count_min.xml reindex=true
-
-
